dd30ba4a902cb0a99f52ad70f43b721285a7035d
Fish <nfisher7@mit.edu>
2023-05-11 17:09:49 -0400

    States 1 & 2 fully working

---
+++ state_2/state_2.ino/state_2.ino.ino
@@ -0,0 +1,183 @@
+#define START 1
+#define GAP_LEFT 2
+#define TANK_RIGHT 3
+#define CURVY 4
+#define LOOP 5
+
+const int left_sens = A0;
+const int mid_sens = A1;
+const int right_sens = A2;
+
+int threshold_l = 750;
+int threshold_r = 800;
+int threshold_m = 800;
+int state;
+
+const int PWML=11; // Pololu drive A
+const int LIN2=10;
+const int LIN1 =9;
+const int STDBY=8;
+const int RIN1 =7; // Pololu drive B
+const int RIN2 =6;
+const int PWMR =5;
+
+
+int prev = 0;
+int cur_turn = 0;
+float aggr = 0.95;
+
+// Line Sensor Variables
+int l;
+int r;
+int m;
+
+bool on_line = true;
+bool prev_on_line = true;
+int gaps = 0;
+
+
+#define GAP 1;
+#define RIGHT 2;
+#define LEFT 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(PWMR , OUTPUT);
+  pinMode(RIN1 , OUTPUT);
+  pinMode(RIN2 , OUTPUT);
+  pinMode(LIN1 , OUTPUT);
+  pinMode(LIN2 , OUTPUT);
+  pinMode(PWML , OUTPUT);
+  pinMode(STDBY , OUTPUT);
+  digitalWrite(STDBY , HIGH);
+
+  drive(0, 0);
+
+}
+
+
+void loop() {
+  l = sense_l();
+  r = sense_r();
+  m = sense_m();
+  prev_on_line = on_line;
+
+  if (l == 0 && r == 0 && m == 0){
+    on_line = false;
+  } else {
+    on_line = true;
+  }
+
+  if (gaps == 3){
+    if(on_line == true){
+      drive(0, 0);
+    } else{
+      drive(-255, 255);
+    }
+  } else{ 
+
+    if (prev_on_line == true && on_line == false){
+    gaps++;
+    }
+    cur_turn = turn_dir_2();
+    make_turn_2(cur_turn, aggr);
+
+  }
+  delay(5);
+}
+
+void drive(int speedL, int speedR){
+  // Here, speed is an integer from -255 to 255
+  motorWrite(speedL, LIN1, LIN2, PWML);
+  motorWrite(speedR, RIN1, RIN2, PWMR);
+}
+
+void motorWrite(int spd, int pin_IN1 , int pin_IN2 , int pin_PWM){
+  if (spd < 0){
+    digitalWrite(pin_IN1 , HIGH); // go one way
+    digitalWrite(pin_IN2 , LOW);
+  }
+  else{
+    digitalWrite(pin_IN1 , LOW); // go the other way
+    digitalWrite(pin_IN2 , HIGH);
+  }
+  analogWrite(pin_PWM , abs(spd));
+}
+
+int sense_l() {
+  int s = analogRead(left_sens);
+  Serial.print("Left: ");
+  Serial.println(s);
+  if (s > threshold_l) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+int sense_r() {
+  int s = analogRead(right_sens);
+  Serial.print("Right: ");
+  Serial.println(s);
+  if (s > threshold_r) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+int sense_m() {
+  int s = analogRead(mid_sens);
+  Serial.print("Mid: ");
+  Serial.println(s);
+  if (s > threshold_m) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+int turn_dir_2() {
+  if (l == 1 && r == 1 && m == 1){
+    return 0; //straight
+  }
+  else if (l == 0 && r == 1 && m ==1){
+    return 1; // slight right
+  }
+  else if (l == 0 && r == 1 && m ==0){
+    return 2; // big right
+  }
+  else if (l == 1 && r == 0 && m ==1){
+    return -1; // slight left
+  }
+  else if (l == 1 && r == 0 && m ==0){
+    return -2; // big left
+  }
+  else if (l==0 && r == 0 && m == 0){
+    return 0;
+  }
+  else{
+    return 0;
+  }
+}
+
+
+
+void make_turn_2(int turn, float aggr){
+  if (turn == 0){
+    drive(170, 170);
+  }
+  if (turn == 1){
+    drive(120, 120 - aggr*96);
+  }
+  if (turn == 2) {
+    drive(90 + aggr*160, 90 - aggr*160);
+  }
+  if (turn == -1){
+    drive(120 - aggr*120, 120 + aggr*120);
+  }
+  if (turn == -2) {
+    drive(90 - aggr*160, 90 + aggr*160);
+  }
+}
--- tank/tank.ino
+++ tank/tank.ino
@@ -1,16 +1,16 @@
-#define START 1;
-#define GAP_LEFT 2;
-#define TANK_RIGHT 3;
-#define CURVY 4;
-#define LOOP 5;
+#define START 1
+#define GAP_LEFT 2
+#define TANK_RIGHT 3
+#define CURVY 4
+#define LOOP 5
 
 const int left_sens = A0;
 const int mid_sens = A1;
 const int right_sens = A2;
 
-int threshold_l = 600;
-int threshold_r = 600;
-int threshold_m = 700;
+int threshold_l = 750;
+int threshold_r = 800;
+int threshold_m = 800;
 int state;
 
 const int PWML=11; // Pololu drive A
@@ -43,6 +43,10 @@ double avg_l;
 double avg_r;
 double avg_m;
 
+bool on_line = true;
+bool prev_on_line = true;
+int gaps = 0;
+
 
 #define GAP 1;
 #define RIGHT 2;
@@ -61,43 +65,69 @@ void setup() {
   pinMode(STDBY , OUTPUT);
   digitalWrite(STDBY , HIGH);
 
+  drive(0, 0);
+
   state = START;
   start_time = millis();
 }
 
-  cur_turn = turn_dir(prev);
-  prev = cur_turn;
-  update_prev_turn(cur_turn);
-  make_turn(cur_turn, aggr);
-  delay(25);
 
 void loop() {
   l = sense_l();
   r = sense_r();
   m = sense_m();
-  update_sens(int l, prev_left);
-  update_sens(int r, prev_right);
-  update_sens(int m, prev_mid);
+
+  prev_on_line = on_line;
+
+  if (l == 0 && r == 0 && m == 0){
+    on_line = false;
+  } else {
+    on_line = true;
+  }
 
   switch (state){
     case START:
       simple();
-      if (millis() - start_time > 20000) state = GAP_LEFT;
+
+      if (millis() - start_time > 23700){
+        state = GAP_LEFT;
+        start_time = millis();
+      } else if (millis() - start_time > 17000){
+        threshold_l = 750;
+        threshold_r = 800;
+        threshold_m = 800;
+      } else if (millis() - start_time > 12500){
+        threshold_l = 600;
+        threshold_r = 600;
+        threshold_m = 650;
+      }
       break;
+
 
     case GAP_LEFT:
-      cur_turn = turn_dir(prev);
-      prev = cur_turn;
-      avg_l = get_avg(prev_left, 20);
-      avg_r = get_avg(prev_right, 20);
-      avg_m = get_avg(prev_mid, 20);
+      if (gaps == 3){
+        if(on_line == true){
+          state = TANK_RIGHT;
+          drive(0, 0);
+        } else{
+          drive(-255, 255);
+        }
+      }
+      else{ 
+        if (prev_on_line == true && on_line == false){
+        gaps++;
+        }
+        cur_turn = turn_dir_2();
+        make_turn_2(cur_turn, aggr);
 
-      update_prev_turn(cur_turn);
-      make_turn(cur_turn, aggr);
+      }
       break;
     
+
     case TANK_RIGHT:
-      // BLuh
+      cur_turn = turn_dir_3(prev);
+      prev = cur_turn;
+      make_turn(cur_turn, aggr);
       break;
 
     case CURVY:
@@ -114,13 +144,13 @@ void loop() {
       break;
   }
   
-  delay(25);
+  delay(5);
 }
 
 void simple(){
   cur_turn = turn_dir(prev);
   prev = cur_turn;
-  update_prev_turn(cur_turn);
+  //update_prev_turn(cur_turn);
   make_turn(cur_turn, aggr);
 }
 
@@ -144,7 +174,9 @@ void motorWrite(int spd, int pin_IN1 , int pin_IN2 , int pin_PWM){
 
 int sense_l() {
   int s = analogRead(left_sens);
-  if (s > 600) {
+  Serial.print("Left: ");
+  Serial.println(s);
+  if (s > threshold_l) {
     return 1;
   } else {
     return 0;
@@ -152,7 +184,9 @@ int sense_l() {
 }
 int sense_r() {
   int s = analogRead(right_sens);
-  if (s > 600) {
+  Serial.print("Right: ");
+  Serial.println(s);
+  if (s > threshold_r) {
     return 1;
   } else {
     return 0;
@@ -160,7 +194,9 @@ int sense_r() {
 }
 int sense_m() {
   int s = analogRead(mid_sens);
-  if (s > 700) {
+  Serial.print("Mid: ");
+  Serial.println(s);
+  if (s > threshold_m) {
     return 1;
   } else {
     return 0;
@@ -184,10 +220,10 @@ int turn_dir(int prev_turn) {
     return -2; // big left
   }
   else if (l==0 && r == 0 && m == 0){
-    if (prev_turn == 2) {
+    if (prev_turn == 2 || prev_turn == 1) {
       return 3;
     }
-    else if (prev_turn == -2) {
+    else if (prev_turn == -2 || prev_turn == -1) {
       return -3;
     }
     return prev_turn; // continue last motion
@@ -197,48 +233,118 @@ int turn_dir(int prev_turn) {
   }
 }
 
+int turn_dir_2() {
+  if (l == 1 && r == 1 && m == 1){
+    return 0; //straight
+  }
+  else if (l == 0 && r == 1 && m ==1){
+    return 1; // slight right
+  }
+  else if (l == 0 && r == 1 && m ==0){
+    return 2; // big right
+  }
+  else if (l == 1 && r == 0 && m ==1){
+    return -1; // slight left
+  }
+  else if (l == 1 && r == 0 && m ==0){
+    return -2; // big left
+  }
+  else if (l==0 && r == 0 && m == 0){
+    return 0;
+  }
+  else{
+    return 0;
+  }
+}
+
+int turn_dir_3(int prev_turn) {
+  if (l == 1 && r == 1 && m == 1){
+    return 1; //slight right
+  }
+  else if (l == 0 && r == 1 && m ==1){
+    return 2; // big right
+  }
+  else if (l == 0 && r == 1 && m ==0){
+    return 3; // huge right
+  }
+  else if (l == 1 && r == 0 && m ==1){
+    return 0; // straight
+  }
+  else if (l == 1 && r == 0 && m ==0){
+    return -2; // big left
+  }
+  else if (l==0 && r == 0 && m == 0){
+    if (prev_turn == -2){
+      return -3;
+    } else{
+      return 3; // huge right
+    }
+  }
+  else{
+    return 0;
+  }
+}
+
 void make_turn(int turn, float aggr){
   if (turn == 0){
-    drive(230, 230);
+    drive(255, 255);
   }
   if (turn == 1){
-    drive(150 + aggr*96, 150 - aggr*96);
+    drive(255, 70);
   }
   if (turn == 2) {
-    drive(128 + aggr*128, 128 - aggr*128);
+    drive(255, 0);
   }
   if (turn == 3) {
-    drive(255, -100);
+    drive(255, -120);
   }
   if (turn == -1){
-    drive(150 - aggr*96, 150 + aggr*96);
+    drive(70, 255);
   }
   if (turn == -2) {
-    drive(128 - aggr*128, 128 + aggr*128);
+    drive(0, 255);
   }
   if (turn == -3) {
-    drive(-100, 255);
+    drive(-120, 255);
   }
 }
 
-void update_prev_turn(int cur_turn){
-  memcpy(prev_turn, &prev_turn[1], sizeof(prev_turn) - sizeof(int));
-  prev_turn[9] =  cur_turn;
-}
-
-void update_sens(int val, int sensor[]){
-  memcpy(sensor, &sensor[1], sizeof(sensor) - sizeof(int));
-  sensor[19] = val;
-}
-
-double get_avg(int arr[], int size){
-  double avg;
-  int sum = 0;
-
-  for (int i = 0; i < size; i++){
-    sum += arr[i];
+void make_turn_2(int turn, float aggr){
+  if (turn == 0){
+    drive(170, 170);
   }
-  avg = double(sum)/size;
+  if (turn == 1){
+    drive(120, 120 - aggr*96);
+  }
+  if (turn == 2) {
+    drive(90 + aggr*160, 90 - aggr*160);
+  }
+  if (turn == -1){
+    drive(120 - aggr*120, 120 + aggr*120);
+  }
+  if (turn == -2) {
+    drive(90 - aggr*160, 90 + aggr*160);
+  }
+}
+
+// void update_prev_turn(int cur_turn){
+//   memcpy(prev_turn, &prev_turn[1], sizeof(prev_turn) - sizeof(int));
+//   prev_turn[9] =  cur_turn;
+// }
+
+// void update_sens(int val, int sensor[]){
+//   memcpy(sensor, &sensor[1], sizeof(sensor) - sizeof(int));
+//   sensor[19] = val;
+// }
+
+// double get_avg(int arr[], int size){
+//   double avg;
+//   int sum = 0;
+
+//   for (int i = 0; i < size; i++){
+//     sum += arr[i];
+//   }
+//   avg = double(sum)/size;
   
-  return avg;
-}
+//   return avg;
+// }
